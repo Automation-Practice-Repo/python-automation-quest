0bc0724df79deab6877717e4de419b9ad23a9a69
hari <hari@gm.com>
2026-03-10 11:39:49 +0530

    Level 1 completed - Python first script challenge

---
+++ level_01_first_script/automation_mission.py
@@ -0,0 +1,7 @@
+Name = "harish"
+Role = "QA Automation Engineer"
+Mission = "Master Python automation"
+
+print(f'Name: {Name}')
+print(f'Role: {Role}')
+print(f'Mission: {Mission}')
---
+++ level_01_first_script/first_script.py
@@ -0,0 +1,9 @@
+print('Hello World')
+
+
+
+# first Python script
+
+print("Python Automation Quest Started")
+print("I am becoming a Python automation engineer")
+print("Level 1 completed successfully")
