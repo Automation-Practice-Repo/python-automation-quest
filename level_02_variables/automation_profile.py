a3c2aaea7dc99ddb74db1394ebd780dfd5c7a468
hari <hari@gm.com>
2026-03-10 15:21:38 +0530

    Level 02: Variables and data types challenge

---
+++ level_02_variables/automation_profile.py
@@ -0,0 +1,11 @@
+name = 'hari'
+role = "tester"
+experience = 3
+primary_language = "python"
+goal = 'Become a world class automation engineer'
+
+print(f'name:', {name})
+print(f'role:', {role})
+print(f'experience:', {experience})
+print(f'primary_language:', {primary_language})
+print(f'goal:', {goal})
---
+++ level_02_variables/variables_demo.py
@@ -0,0 +1,11 @@
+# variable examples
+
+name = "Hareesh"
+experience = 5
+salary_expectation = 12.5
+is_automation_engineer = True
+
+print("Name:", name)
+print("Experience:", experience)
+print("Expected Salary:", salary_expectation)
+print("Automation Engineer:", is_automation_engineer)
